1297464088b75acf35412eafe63b957450d07e69
Chris Welch <christopher.david.welch@gmail.com>
2022-11-15 18:22:34 +1300

    did some dall-e

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Espis',
+    img: require('@site/static/img/espis.png').default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -14,8 +14,18 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Arkenreln',
+    img: require('@site/static/img/arkenreln.png').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Sands of the First Sin',
+    img: require('@site/static/img/first-sin.png').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -24,8 +34,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Highhaven-In-Solitude',
+    img: require('@site/static/img/solitude.png').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -33,13 +43,24 @@ const FeatureList = [
       </>
     ),
   },
+  {
+    title: 'The Abyssal Crown',
+    img: require('@site/static/img/magical-tragedy.png').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+  
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({img, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={img} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
